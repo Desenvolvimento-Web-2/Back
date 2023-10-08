c9f83d906749ce65dc5fe5a7865a178ae3d5925f
Alexandre Diano <42250970+AlexandreDiano@users.noreply.github.com>
2023-10-08 19:58:13 -0300

    Adicionando DTO dos Pedidos

---
+++ src/main/java/br/net/trabalho/api/rest/dto/pedido/NovoPedidoRequestDTO.java
@@ -0,0 +1,24 @@
+package br.net.trabalho.api.rest.dto.pedido;
+
+import java.util.List;
+
+public class NovoPedidoRequestDTO {
+    private Long clienteId;
+    private List<RoupasNovoPedidoDTO> roupas;
+
+    public Long getClienteId() {
+        return clienteId;
+    }
+
+    public void setClienteId(Long clienteId) {
+        this.clienteId = clienteId;
+    }
+
+    public List<RoupasNovoPedidoDTO> getRoupas() {
+        return roupas;
+    }
+
+    public void setRoupas(List<RoupasNovoPedidoDTO> roupas) {
+        this.roupas = roupas;
+    }
+}
